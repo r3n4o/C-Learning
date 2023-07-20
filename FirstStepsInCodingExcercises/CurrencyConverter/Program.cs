bd944c3b7e174dabf968f2f211944858eea0ecb4
renire66@gmail.com <renire66@gmail.com>
2023-07-20 16:51:00 +0300

    Second lecture solution

---
+++ FirstStepsInCodingExcercises/CurrencyConverter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CurrencyConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
